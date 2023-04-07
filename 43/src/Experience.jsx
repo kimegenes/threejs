26c8f95cfd10487c1f5fc46b2bf018040b589925
Kim Egenes <kimegenes@hotmail.com>
2023-04-07 11:37:53 +0200

    lession 43

---
+++ 43/src/Experience.jsx
@@ -0,0 +1,69 @@
+import { useThree, extend } from '@react-three/fiber'
+import { MeshReflectorMaterial, Float, Text, Html, PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
+
+export default function Experience()
+{
+    const cube = useRef()
+    const sphere = useRef()
+    const { camera, gl } = useThree()
+
+    return <>
+        <OrbitControls makeDefault />
+    
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
+
+        <PivotControls 
+            anchor={[0,0,0]} 
+            depthTest={false}
+        >
+            <mesh ref={ sphere } position-x={ - 2 } >
+                <sphereGeometry />
+                <meshStandardMaterial color="orange" />
+                <Html 
+                    wrapperClass='label' 
+                    position={[1,1,0]} 
+                    center
+                    distanceFactor={ 8}
+                    occlude={[ cube, sphere ]}
+                    
+                >That's a sphere 👍</Html>
+            </mesh>
+        </PivotControls>
+        <Float
+            speed={ 5 }
+            floatIntensity={2}
+        >
+            <Text
+                font="./bangers-v20-latin-regular.woff"
+                fontSize={ 1 }
+                color={'salmon'}
+                position-y={2}
+                maxWidth={2}
+                textAlign='center'
+            >
+                I LOVE R3F
+                
+            </Text>
+        </Float>
+        
+        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+        <TransformControls object={ cube }  mode="translate"/>
+
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <MeshReflectorMaterial 
+                resolution={ 512 }
+                blur={ [ 1000, 1000 ] }
+                mixBlur={ 1 }
+                mirror={ 0.5 }
+                color="greenyellow"
+            />
+        </mesh>
+
+    </>
+}
